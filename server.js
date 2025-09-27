d9ffc0c61953ce3d15ae774b2a101e9acf9ad04a
Muoki-Mary <muokimary156@gmail.com>
2025-09-27 12:54:38 +0300

    Fix: connect task routes to server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const db = require('./database/connection');
+const taskRoutes = require('./routes/tasks');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +9,9 @@ const PORT = process.env.PORT || 3000;
 // middleware
 app.use(express.json());
 app.use(cors());
+
+// routes
+app.use('/api/tasks', taskRoutes);
 
 app.get('/', (req, res) => {
   res.json({ 
